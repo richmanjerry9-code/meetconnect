f1edb3a7fdeb38e52ee59f15b351225dd4c8c318
richmanjerry <richmanjerry9@gmail.com>
2025-10-23 13:32:03 +0300

    Added full 47 counties locations.js

--- data/locations.js
+++ data/locations.js
@@ -97,3 +97,399 @@ export const Nairobi = {
   Utawala: ['Utawala', 'Amani', 'Mihango', 'Shooters', 'Stage 75'],
   Westlands: ['Westlands'],
 };
+// locations.js
+
+export const Baringo = {
+  'Baringo Town': ['Baringo Town'],
+  'Eldama Ravine': ['Eldama Ravine'],
+  Kabarnet: ['Kabarnet'],
+  Marigat: ['Marigat'],
+  Mogotio: ['Mogotio'],
+  Ratat: ['Ratat'],
+  Sirisia: ['Sirisia'],
+};
+
+export const Bomet = {
+  Bomet: ['Bomet'],
+  Chebole: ['Chebole'],
+  Chemaner: ['Chemaner'],
+  Kaboson: ['Kaboson'],
+  Kaplong: ['Kaplong'],
+  Kanoin: ['Kanoin'],
+  Longisa: ['Longisa'],
+  Mogogosiek: ['Mogogosiek'],
+  Silibwet: ['Silibwet'],
+  Sotik: ['Sotik'],
+};
+
+export const Bungoma = {
+  Bumala: ['Bumala'],
+  'Bungoma Town': ['Bungoma Town'],
+  Kimilili: ['Kimilili'],
+  Misikhu: ['Misikhu'],
+  Webuye: ['Webuye'],
+};
+
+export const Busia = {
+  Busia: ['Busia'],
+};
+
+export const ElgeyoMarakwet = {
+  Iten: ['Iten'],
+  Kapsowar: ['Kapsowar'],
+  Metkei: ['Metkei'],
+};
+
+export const Embu = {
+  'Embu Town': ['Embu Town'],
+  Kiritiri: ['Kiritiri'],
+  'Mbeere North': ['Mbeere North'],
+  'Mbeere South': ['Mbeere South'],
+  Runyenjes: ['Runyenjes'],
+  Siakago: ['Siakago'],
+};
+
+export const Garissa = {
+  'Garissa Town': ['Garissa Town'],
+};
+
+export const Homabay = {
+  'Homabay Town': ['Homabay Town'],
+  'Kendu Bay': ['Kendu Bay'],
+  Ndhiwa: ['Ndhiwa'],
+  Oyugis: ['Oyugis'],
+  Rangwe: ['Rangwe'],
+  'Rodi Kopany': ['Rodi Kopany'],
+  'Rusinga Island': ['Rusinga Island'],
+};
+
+export const Isiolo = {
+  'Isiolo Town': ['Isiolo Town'],
+};
+
+export const Kajiado = {
+  Illasit: ['Illasit'],
+  Isinya: ['Isinya'],
+  'Kajiado Town': ['Kajiado Town'],
+  Kimana: ['Kimana'],
+  Loitokitok: ['Loitokitok'],
+  Namanga: ['Namanga'],
+};
+
+export const Kakamega = {
+  Butere: ['Butere'],
+  'Kakamega Town': ['Kakamega Town'],
+  Khayega: ['Khayega'],
+  Lubao: ['Lubao'],
+  Lugari: ['Lugari'],
+  Malava: ['Malava'],
+  Mumias: ['Mumias'],
+  Navakholo: ['Navakholo'],
+};
+
+export const Kericho = {
+  Chepsir: ['Chepsir'],
+  Kapsoit: ['Kapsoit'],
+  Kapsuser: ['Kapsuser'],
+  'Kericho Town': ['Kericho Town'],
+  Kipkelion: ['Kipkelion'],
+  Litein: ['Litein'],
+  Londiani: ['Londiani'],
+  Sosiot: ['Sosiot'],
+};
+
+export const Kiambu = {
+  'Gatundu South': ['Gatundu South'],
+  Githunguri: ['Githunguri'],
+  Kiambaa: ['Kiambaa'],
+  Kiambu: ['Kiambu'],
+  Lari: ['Lari'],
+  Limuru: ['Limuru'],
+  Wanginge: ['Wanginge'],
+};
+
+export const Kilifi = {
+  Kikambala: ['Kikambala'],
+  'Kilifi Town': ['Kilifi Town'],
+  Malindi: ['Malindi'],
+  Mariakani: ['Mariakani'],
+  Mazeras: ['Mazeras'],
+};
+
+export const Kirinyaga = {
+  Kerugoya: ['Kerugoya'],
+  Kibirigwi: ['Kibirigwi'],
+  Makutano: ['Makutano'],
+  Mwea: ['Mwea'],
+  Sagana: ['Sagana'],
+};
+
+export const Kisii = {
+  Kisii: ['Kisii'],
+  Birongo: ['Birongo'],
+  Bobaracho: ['Bobaracho'],
+  Gucha: ['Gucha'],
+  Kegati: ['Kegati'],
+  Kenyenya: ['Kenyenya'],
+  Keroka: ['Keroka'],
+  Keumbu: ['Keumbu'],
+  'Kisii Town': ['Kisii Town'],
+  Nyamarambe: ['Nyamarambe'],
+  Nyansiongo: ['Nyansiongo'],
+  Senuka: ['Senuka'],
+};
+
+export const Kisumu = {
+  Kisumu: ['Kisumu'],
+  Ahero: ['Ahero'],
+  Katito: ['Katito'],
+  Maseno: ['Maseno'],
+  Nyakach: ['Nyakach'],
+  Sondu: ['Sondu'],
+};
+
+export const Kitui = {
+  Kitui: ['Kitui'],
+  'Kitui Town': ['Kitui Town'],
+  Mutomo: ['Mutomo'],
+  Mwingi: ['Mwingi'],
+};
+
+export const Kwale = {
+  Kwale: ['Kwale'],
+  Diani: ['Diani'],
+  Kinango: ['Kinango'],
+  'Kwale Town': ['Kwale Town'],
+  'Lunga Lunga': ['Lunga Lunga'],
+  'Mackinnon Road': ['Mackinnon Road'],
+  Msambweni: ['Msambweni'],
+};
+
+export const Laikipia = {
+  Laikipia: ['Laikipia'],
+  Nanyuki: ['Nanyuki'],
+  Ndaragwa: ['Ndaragwa'],
+  Nyahururu: ['Nyahururu'],
+  Rumuruti: ['Rumuruti'],
+};
+
+export const Lamu = {
+  Lamu: ['Lamu'],
+  Mpeketoni: ['Mpeketoni'],
+  Witu: ['Witu'],
+};
+
+export const Machakos = {
+  Machakos: ['Machakos'],
+  Kagundo: ['Kagundo'],
+  'Machakos Junction': ['Machakos Junction'],
+  'Machakos Town': ['Machakos Town'],
+  Masii: ['Masii'],
+  Masinga: ['Masinga'],
+  Matuu: ['Matuu'],
+  Mwala: ['Mwala'],
+  Tala: ['Tala'],
+};
+
+export const Makueni = {
+  Makueni: ['Makueni'],
+  'Wote Town': ['Wote Town'],
+  Emali: ['Emali'],
+  'Kibwezi Town': ['Kibwezi Town'],
+  Makindu: ['Makindu'],
+  'Mtito Andei': ['Mtito Andei'],
+  'Sultan Hamud': ['Sultan Hamud'],
+};
+
+export const Mandera = {
+  Mandera: ['Mandera'],
+  'El-Wak Town': ['El-Wak Town'],
+  'Mandera Town': ['Mandera Town'],
+};
+
+export const Marsabit = {
+  Marsabit: ['Marsabit'],
+  'Marsabit Town': ['Marsabit Town'],
+  'Moyale Town': ['Moyale Town'],
+};
+
+export const Meru = {
+  Meru: ['Meru'],
+  Igembe: ['Igembe'],
+  Kaaga: ['Kaaga'],
+  Kangeta: ['Kangeta'],
+  Kianjai: ['Kianjai'],
+  Maua: ['Maua'],
+  'Meru Town': ['Meru Town'],
+  'Nkubu Town': ['Nkubu Town'],
+  Tigania: ['Tigania'],
+};
+
+export const Migori = {
+  Migori: ['Migori'],
+  Awendo: ['Awendo'],
+  'Migori Town': ['Migori Town'],
+  Rongo: ['Rongo'],
+};
+
+export const Mombasa = {
+  Mombasa: ['Mombasa'],
+  Changamwe: ['Changamwe'],
+  Jomvu: ['Jomvu'],
+  Kisauni: ['Kisauni'],
+  Likoni: ['Likoni'],
+  Miritini: ['Miritini'],
+  Mtongwe: ['Mtongwe'],
+  Mvita: ['Mvita'],
+  'Port Reitz': ['Port Reitz'],
+  Ukunda: ['Ukunda'],
+  Watamu: ['Watamu'],
+};
+
+export const Muranga = {
+  Muranga: ['Muranga'],
+  Gatura: ['Gatura'],
+  Githumu: ['Githumu'],
+  Kandara: ['Kandara'],
+  Kahuro: ['Kahuro'],
+  Kangema: ['Kangema'],
+  Kenol: ['Kenol'],
+  Kigumo: ['Kigumo'],
+  Kiharu: ['Kiharu'],
+  Kiriani: ['Kiriani'],
+  Makuyu: ['Makuyu'],
+  Maragua: ['Maragua'],
+  Mathioya: ['Mathioya'],
+  'Muranga Town': ['Muranga Town'],
+  Sabasaba: ['Sabasaba'],
+};
+
+export const Nakuru = {
+  Nakuru: ['Nakuru'],
+  Elburgon: ['Elburgon'],
+  Gilgil: ['Gilgil'],
+  'Mai Mahiu': ['Mai Mahiu'],
+  Molo: ['Molo'],
+  Naivasha: ['Naivasha'],
+  Njoro: ['Njoro'],
+  Salgaa: ['Salgaa'],
+};
+
+export const Nandi = {
+  Nandi: ['Nandi'],
+  Kaiboi: ['Kaiboi'],
+  Kapsabet: ['Kapsabet'],
+  Lessos: ['Lessos'],
+  Mosoriot: ['Mosoriot'],
+  'Nandi Hills': ['Nandi Hills'],
+  Sangalo: ['Sangalo'],
+};
+
+export const Narok = {
+  Narok: ['Narok'],
+  Kilgoris: ['Kilgoris'],
+  'Masai Mara': ['Masai Mara'],
+  'Narok Town': ['Narok Town'],
+};
+
+export const Nyamira = {
+  Nyamira: ['Nyamira'],
+};
+
+export const Nyandarua = {
+  Nyandarua: ['Nyandarua'],
+  'Ol Kalou': ['Ol Kalou'],
+};
+
+export const Nyeri = {
+  Nyeri: ['Nyeri'],
+  Chaka: ['Chaka'],
+  Karatina: ['Karatina'],
+  Kieni: ['Kieni'],
+  'Mukurwe-ini': ['Mukurwe-ini'],
+  Mweiga: ['Mweiga'],
+  'Naro Moru': ['Naro Moru'],
+  'Nyeri Town': ['Nyeri Town'],
+  Othaya: ['Othaya'],
+};
+
+export const Samburu = {
+  Samburu: ['Samburu'],
+  Maralal: ['Maralal'],
+};
+
+export const Siaya = {
+  Siaya: ['Siaya'],
+  Bondo: ['Bondo'],
+  Ugunja: ['Ugunja'],
+  Usenge: ['Usenge'],
+};
+
+export const TaitaTaveta = {
+  'Taita Taveta': ['Taita Taveta'],
+  'Maungu Town': ['Maungu Town'],
+  Mwatate: ['Mwatate'],
+  Taveta: ['Taveta'],
+  Voi: ['Voi'],
+  Wundanyi: ['Wundanyi'],
+};
+
+export const TanaRiver = {
+  'Tana River': ['Tana River'],
+  'Bura Town': ['Bura Town'],
+  'Galole Town': ['Galole Town'],
+  Garsen: ['Garsen'],
+};
+
+export const TharakaNithi = {
+  'Tharaka Nithi': ['Tharaka Nithi'],
+  Chogoria: ['Chogoria'],
+  Chuka: ['Chuka'],
+  Gatunga: ['Gatunga'],
+  Itugururu: ['Itugururu'],
+  Kamaende: ['Kamaende'],
+  Kanjuki: ['Kanjuki'],
+  Kathenaugi: ['Kathenaugi'],
+  Kathwana: ['Kathwana'],
+  Maara: ['Maara'],
+  Marimanti: ['Marimanti'],
+};
+
+export const TransNzoia = {
+  'Trans Nzoia': ['Trans Nzoia'],
+  Endebess: ['Endebess'],
+  Kiminini: ['Kiminini'],
+  Kitale: ['Kitale'],
+  Sibanga: ['Sibanga'],
+};
+
+export const Turkana = {
+  Turkana: ['Turkana'],
+  Kakuma: ['Kakuma'],
+  Lodwar: ['Lodwar'],
+  Lokichar: ['Lokichar'],
+  Lokichogio: ['Lokichogio'],
+};
+
+export const UasinGishu = {
+  'Uasin Gishu': ['Uasin Gishu'],
+  Eldoret: ['Eldoret'],
+  "Moi's Bridge": ["Moi's Bridge"],
+};
+
+export const Vihiga = {
+  Vihiga: ['Vihiga'],
+  Chavakali: ['Chavakali'],
+  Luanda: ['Luanda'],
+  Mbale: ['Mbale'],
+};
+
+export const Wajir = {
+  Wajir: ['Wajir'],
+  'Wajir Town': ['Wajir Town'],
+};
+
+export const WestPokot = {
+  'West Pokot': ['West Pokot'],
+  Kapenguria: ['Kapenguria'],
+};
