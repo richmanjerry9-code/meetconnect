64fada4df46ca1e8dadf72f4f74b787dc4ef5eec
richmanjerry <richmanjerry9@gmail.com>
2025-11-09 17:28:29 +0300

    Update package.json and next.config.js

--- next.config.js
+++ next.config.js
@@ -1,19 +1,10 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  env: {
-    FIREBASE_API_KEY: process.env.FIREBASE_API_KEY,
-    FIREBASE_AUTH_DOMAIN: process.env.FIREBASE_AUTH_DOMAIN,
-    // add others similarly
-  },
-  images: {
-    remotePatterns: [
-      {
-        protocol: 'https',
-        hostname: '**', // allow all external hosts (optional)
-      },
-    ],
+  experimental: {
+    turbo: false, // disables Turbopack
   },
 };
 
 module.exports = nextConfig;
+
 
